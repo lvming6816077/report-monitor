4b0439893af2cd7f6107df56d12c0b677c05ff21
lvming@ccolsn.com <lvming@ccolsn.com>
2022-07-22 10:45:31 +0800

    日志模块

--- report-monitor-server/src/log/log.controller.ts
+++ report-monitor-server/src/log/log.controller.ts
@@ -1,49 +1,67 @@
-import { Body, Controller, Query, Get, Param, Post, Request, HttpException } from '@nestjs/common';
-import { LogService,resultVo } from './log.service';
-import {formatData} from 'src/utils/report/formatReportData'
+import { Body, Controller, Query, Get, Param, Post, Request, HttpException, UseGuards } from '@nestjs/common';
+import { LogService, resultVo } from './log.service';
+import { formatData } from 'src/utils/report/formatReportData'
 
 import * as moment from 'moment'
-import { Request as _Request} from 'express';
+import { Request as _Request } from 'express';
 import { PointService } from 'src/point/point.service';
+import { JwtAuthGuard } from 'src/config/jwt-config/jwtAuth.guard';
 
 
 
 @Controller('log')
 export class LogController {
-  constructor(
-    private readonly logService: LogService,
-    
-    private readonly pointService: PointService,
+    constructor(
+        private readonly logService: LogService,
+
+        private readonly pointService: PointService,
     ) { }
 
-  @Post('create/:tc')
-  async createLog(@Body() dto: any, @Request() req: _Request,@Param('tc') tc: string) {
+    @Post('create/:tc')
+    async createLog(@Body() dto: any, @Request() req: _Request, @Param('tc') tc: string) {
 
-    //   const tc = req.query?.tc
+        //   const tc = req.query?.tc
 
-      if (!tc) {
-        throw new HttpException('tid缺失', 200);
-      }
+        if (!tc) {
+            throw new HttpException('tid缺失', 200);
+        }
 
-      const tag = await this.pointService.findOneTagByCode(tc as string)
+        const tag = await this.pointService.findOneTagByCode(tc as string)
 
-      if (!tag) {
-        throw new HttpException('tid不存在', 200);
-      }
+        if (!tag) {
+            throw new HttpException('tid不存在', 200);
+        }
 
-      return await this.logService.create(JSON.stringify(dto),tag._id);
-  }
+        return await this.logService.create(JSON.stringify(dto), tag._id);
+    }
 
-  @Get('list')
-  async getLogList(@Request() req: _Request) {
-    const list = await this.logService.findAll();
+    @UseGuards(JwtAuthGuard)
+    @Get('list')
+    async getLogList(@Request() req: _Request) {
+        const tagList = await (await this.pointService.findAllTags()).filter(i => i.code)
 
-    
-    return list.filter(i=>i.str)
-  }
+        let list = []
 
-  //   @Delete(':id')
-  //   async delete(@Param('id') id: string) {
-  //     return this.catsService.delete(id);
-  //   }
+        for (var i = 0; i < tagList.length; i++) {
+            let l = await this.logService.findAllByTagId(tagList[i]._id)
+            list.push(l)
+        }
+        // 拍平
+        function flatter(arr) {
+            if (!arr.length) return;
+            return arr.reduce(
+                (pre, cur) =>
+                    Array.isArray(cur) ? [...pre, ...flatter(cur)] : [...pre, cur],
+                []
+            );
+        }
+
+
+        return flatter(list)
+    }
+
+    //   @Delete(':id')
+    //   async delete(@Param('id') id: string) {
+    //     return this.catsService.delete(id);
+    //   }
 }
--- report-monitor-server/src/log/log.service.ts
+++ report-monitor-server/src/log/log.service.ts
@@ -1,4 +1,4 @@
-import { Injectable,Inject } from "@nestjs/common";
+import { Injectable,Inject, Scope } from "@nestjs/common";
 import { InjectModel } from '@nestjs/mongoose';
 import { Log, LogDocument } from './schemas/log.schema';
 import { Model } from 'mongoose';
@@ -10,16 +10,20 @@ import { RedisInstanceService } from "src/config/redis-config/redis.service";
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger  } from 'winston';
 import { Point,PointDocument } from "src/point/schemas/point.schema";
+
 export type resultVo = {
     list?:Log[],
     desc?:string
 };
-@Injectable()
+
+
 export class LogService {
     constructor(
         @InjectModel(Log.name) private readonly logModel: Model<LogDocument>, 
         // private readonly pointService: PointService,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+
+        // @Inject(REQUEST) private readonly req: CURUSER
         ) { 
 
         }
@@ -29,8 +33,9 @@ export class LogService {
             return await this.logModel.create({ str: str,tag:tid });
         }
 
-        async findAll(): Promise<Log[]> {
-            let list = await this.logModel.find({ }).exec();
+        async findAllByTagId(tid:string): Promise<Log[]> {
+            let list = await this.logModel.find({ tag:tid }).exec();
+            
     
             return list
         }
