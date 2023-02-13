8640445a15d6a1c8c40c749f163af742d8959adc
lvming@ccolsn.com <lvming@ccolsn.com>
2023-02-13 15:42:23 +0800

    邮件

--- report-monitor-server/src/warning/warning.controller.ts
+++ report-monitor-server/src/warning/warning.controller.ts
@@ -15,22 +15,27 @@ import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { JwtAuthGuard } from 'src/config/jwt-config/jwtAuth.guard';
 
+
 import { PaginateResult } from 'mongoose';
 import { DeleteResult } from 'mongodb';
 import { UserService } from 'src/user/user.service';
 import { WarningService } from './warning.service';
 import { CreateWarningDto } from './dto/create-warning.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('warning')
-@UseGuards(JwtAuthGuard)
+
 export class WarningController {
     constructor(
         private readonly warningService: WarningService,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
 
+        private readonly mailerService: MailerService,
+
         private readonly userService: UserService,
     ) {}
-
+    
+    @UseGuards(JwtAuthGuard)
     @Post('addWarningSet')
     async addWarningSet(@Body() dto: CreateWarningDto, @Request() req: any) {
         console.log(req.user);
@@ -41,8 +46,24 @@ export class WarningController {
         return await this.warningService.create(dto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('updateWarningOpen')
     async updateWarningOpen(@Query() query) {
         return await this.warningService.updateWarningOpen(query.id, false);
+    }
+
+    @Post('helpMeSendMail')
+    async helpMeSendMail(@Body() dto: any, @Request() req: any) {
+        
+        return this.mailerService
+                .sendMail({
+                    to: dto.email,
+                    from: 'Report Monitor <monitor@nihaoshijie.com.cn>',
+                    subject: dto.subject,
+                    text: dto.text,
+                })
+                .catch((e) => {
+                    this.logger.error(e), console.log(e);
+                });
     }
 }
